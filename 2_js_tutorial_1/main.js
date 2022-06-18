27b3e71305c9602fe1b824ec31d107fee17fef66
B.S Felipe Jeon <junbs95@gmail.com>
2022-06-18 09:10:15 +0900

    Start plotly

---
+++ 2_js_tutorial_1/main.js
@@ -0,0 +1,20 @@
+'use strict';
+console.log("hello world!")
+console.log(5 / 2)
+
+// elements of const container can be newly defined.. 
+const someContainer = {};
+someContainer.id = 1
+console.log(someContainer)
+
+// loose equality vs strict equality 
+const stringFive = '5';
+const numberFive = 5;
+console.log(stringFive == numberFive);
+console.log(stringFive === numberFive);
+
+let obj = {
+    name: 'john doe',
+    age: 21
+}
+
---
+++ 3_plotly_tutorial/main.js
@@ -0,0 +1,12 @@
+TESTER = document.getElementById('tester');
+let xx, yy;
+xx = [1, 2, 3, 4, 5];
+yy = [1, 2, 4, 8, 10];
+let layout = {
+    title: 'JBS first example',
+}
+let config = {
+    displayModeBar: false
+}
+
+Plotly.newPlot(TESTER, [{ x: xx, y: yy }], layout, config)
